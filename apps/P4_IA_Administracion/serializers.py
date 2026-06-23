9b7caacb3784126c5851c3446a779ed888e8e9f2
brayanjoelrv <ramosvargabrayan@gmail.com>
2026-06-23 06:22:03 -0400

    fix: update TransaccionSerializer to map frontend fields paciente_nombre, concepto, metodo_pago and allow POST without tipo

--- apps/P4_IA_Administracion/serializers.py
+++ apps/P4_IA_Administracion/serializers.py
@@ -14,9 +14,29 @@ class LogAuditoriaSerializer(serializers.ModelSerializer):
         return obj.fecha.strftime("%d/%m/%Y %H:%M:%S")
 
 class TransaccionSerializer(serializers.ModelSerializer):
+    paciente_nombre = serializers.ReadOnlyField(source='paciente.nombre')
+    concepto = serializers.CharField(source='descripcion', required=False, allow_blank=True)
+    metodo_pago = serializers.SerializerMethodField()
+
     class Meta:
         model = Transaccion
-        fields = '__all__'
+        fields = ['id', 'paciente', 'paciente_nombre', 'monto', 'tipo', 'fecha', 'descripcion', 'concepto', 'metodo_pago']
+        extra_kwargs = {'tipo': {'required': False}}
+
+    def get_metodo_pago(self, obj):
+        if 'STRIPE' in obj.descripcion or 'TARJETA' in obj.descripcion: return 'TARJETA'
+        elif 'QR' in obj.descripcion: return 'QR'
+        elif '[' in obj.descripcion and ']' in obj.descripcion:
+            return obj.descripcion.split('[')[-1].split(']')[0]
+        return 'EFECTIVO'
+
+    def create(self, validated_data):
+        if 'tipo' not in validated_data:
+            validated_data['tipo'] = 'PAGO'
+        metodo_pago = self.initial_data.get('metodo_pago', 'EFECTIVO')
+        if 'descripcion' in validated_data:
+            validated_data['descripcion'] = f"{validated_data['descripcion']} [{metodo_pago}]"
+        return super().create(validated_data)
 
 class ComprobanteSerializer(serializers.ModelSerializer):
     class Meta:
